b3be03bbc5d75c74fad605b9aae0f9d2b306bfd8
Asaad <asaad1793.z@gmail.com>
2025-12-11 21:59:34 +0200

    add untracked files

--- schema.py
+++
@@ -1,6 +0,0 @@
-import graphene
-
-class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hello, GraphQL!") #name of the query 'hello'
-
-schema = graphene.Schema(query=Query)
